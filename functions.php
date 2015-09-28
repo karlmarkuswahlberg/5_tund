1953d8e1c30524694fbbecb18dc16625dcb86ba1
Karl Markus Wahlberg <skmw@greeny.cs.tlu.ee>
2015-09-28 12:46:23 +0300

    added example to function.php

--- functions.php
+++ functions.php
@@ -1,1 +1,18 @@
-funktsioonid
+<?php
+		
+	//functions.php
+	//siia tulevad funktsioonid
+	
+	function hello1($name, $age){
+		echo "Tere ".$name." kes on ".$age." aastat vana.";
+	}
+	
+	function hello2($name){
+		return "Tere ".$name; //võtab selle jupi, mis on return järel ja paneb selle järele, mis on all: hello2. Tagastab
+	}
+	
+	echo "<br>";
+	hello1("Juku, 5");
+	echo "<br>";
+	echo hello2("Juhan");
+?>
